c9d9f32d192b4b8bfde7a410a321767c520767df
koala0521 <328762691@qq.com>
2018-08-22 21:06:51 +0800

    新版 1.2.1.1 开发

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 /**
  * 显示菜单
  */
-function showMenu () {
+function showMenu() {
   var prompt = require('@system.prompt');
   var router = require('@system.router');
   var appInfo = require('@system.app').getInfo()
@@ -17,14 +17,19 @@ function showMenu () {
           // 关于
           router.push({
             uri: '/About',
-            params: { name: appInfo.name, icon: appInfo.icon }
+            params: {
+              name: appInfo.name,
+              icon: appInfo.icon
+            }
           })
           break;
         case 2:
           // 取消
           break;
         default:
-          prompt.showToast({ message: 'error' })
+          prompt.showToast({
+            message: 'error'
+          })
       }
     }
   })
@@ -34,20 +39,26 @@ function showMenu () {
  * 创建桌面图标
  * 注意：使用加载器测试`创建桌面快捷方式`功能时，请先在`系统设置`中打开`应用加载器`的`桌面快捷方式`权限
  */
-function createShortcut () {
+function createShortcut() {
   var prompt = require('@system.prompt');
   var shortcut = require('@system.shortcut');
   shortcut.hasInstalled({
     success: function (ret) {
       if (ret) {
-        prompt.showToast({ message: '已创建桌面图标' })
+        prompt.showToast({
+          message: '已创建桌面图标'
+        })
       } else {
         shortcut.install({
           success: function () {
-            prompt.showToast({ message: '成功创建桌面图标' })
+            prompt.showToast({
+              message: '成功创建桌面图标'
+            })
           },
           fail: function (errmsg, errcode) {
-            prompt.showToast({ message: 'error: ' + errcode + '---' + errmsg })
+            prompt.showToast({
+              message: 'error: ' + errcode + '---' + errmsg
+            })
           }
         })
       }
